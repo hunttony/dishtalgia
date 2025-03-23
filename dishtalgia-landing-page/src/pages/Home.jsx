34100e425b3766420b77f0d8d52030084cd92bcb
Antonio Hunt <hunttony468@gmail.com>
2025-03-23 16:30:58 -0500

    update 3-23-25, site image margin increase below fun facts

--- dishtalgia-landing-page/src/pages/Home.jsx
+++ dishtalgia-landing-page/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Reac, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { Container, Typography, Box, Button, Paper } from "@mui/material";
 import { motion } from "framer-motion";
 import { Helmet } from "react-helmet";
